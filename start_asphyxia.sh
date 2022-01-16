4469bcfcc0d617bdb388cf59c649dd4662af3335
tmapes <maplemapes2@gmail.com>
2022-01-16 20:40:18 +0000

    Remove Debug output as it isn't helpful

--- start_asphyxia.sh
+++ start_asphyxia.sh
@@ -5,12 +5,6 @@ MATCHING_PORT="${MATCHING_PORT:=5700}"
 HOST="${HOST:=0.0.0.0}"
 PING_IP_ADDRESS="${PING_IP_ADDRESS:=127.0.0.1}"
 
-EXTRA_OPTS=""
-if [[ -n "$DEBUG_OUTPUT" ]]; then
-  EXTRA_OPTS="$EXTRA_OPTS --dev"
-fi
-
-
 echo "Running on port: $LISTENING_PORT, matching port: $MATCHING_PORT, host: $HOST, pingable ip address: $PING_IP_ADDRESS"
 
-/opt/asphyxia-core -p $LISTENING_PORT -b $HOST -m $MATCHING_PORT -pa $PING_IP_ADDRESS "$EXTRA_OPTS"
+/opt/asphyxia-core -p $LISTENING_PORT -b $HOST -m $MATCHING_PORT -pa $PING_IP_ADDRESS
