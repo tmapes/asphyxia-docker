a13245e78055fa195d2157c1fee8ac3b3b9a1456
tmapes <maplemapes2@gmail.com>
2024-04-14 16:15:05 -0500

    Allow passing additional arguments to asphyxia-core

--- start_asphyxia.sh
+++ start_asphyxia.sh
@@ -5,6 +5,6 @@ MATCHING_PORT="${MATCHING_PORT:=5700}"
 HOST="${HOST:=0.0.0.0}"
 PING_IP_ADDRESS="${PING_IP_ADDRESS:=127.0.0.1}"
 
-echo "Running on port: $LISTENING_PORT, client matching port: $MATCHING_PORT, host: $HOST, pingable ip address: $PING_IP_ADDRESS"
+echo "Running on port: $LISTENING_PORT, client matching port: $MATCHING_PORT, host: $HOST, pingable ip address: $PING_IP_ADDRESS, additional options: $ASPHYXIA_OPTS"
 
-exec /opt/asphyxia/asphyxia-core -p $LISTENING_PORT -b $HOST -m $MATCHING_PORT -pa $PING_IP_ADDRESS
+exec /opt/asphyxia/asphyxia-core -p $LISTENING_PORT -b $HOST -m $MATCHING_PORT -pa $PING_IP_ADDRESS "$ASPHYXIA_OPTS"
